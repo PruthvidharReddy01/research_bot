e76169bfc7c9f874d2ed28d80a2acdf4047316f6
PruthvidharReddy01 <pruthvidharreddykotha@gmail.com>
2026-05-21 17:08:48 +0530

    slack-integration

--- rag.py
+++ rag.py
@@ -14,55 +14,64 @@ import os
 
 
 def create_vectorstore():
-    folder_path = "documents"
+
+    #slack and other knowledge based uploaded files folder paths are stored here for creating vectorss
+    folder_paths = [
+        "documents",
+        "uploaded_docs"
+    ]
+
     all_docs = []
 
-   
-    if not os.path.exists(folder_path):
-        print("Documents folder not found.")
-        return None
+    
+    for folder_path in folder_paths:
 
-    # this read all supported files
-    for file in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, file)
+        if not os.path.exists(folder_path):
+            print(f"{folder_path} folder not found.")
+            continue
 
-        try:
-            if file.endswith(".pdf"):
-                loader = PyPDFLoader(file_path)
+        
+        for file in os.listdir(folder_path):
 
-            elif file.endswith(".docx"):
-                loader = Docx2txtLoader(file_path)
+            file_path = os.path.join(folder_path, file)
 
-            elif file.endswith(".txt"):
-                loader = TextLoader(
-                    file_path,
-                    encoding="utf-8"
-                )
+            try:
 
-            elif file.endswith(".csv"):
-                loader = CSVLoader(file_path)
+                if file.endswith(".pdf"):
+                    loader = PyPDFLoader(file_path)
 
-            elif file.endswith(".xlsx"):
-                loader = UnstructuredExcelLoader(file_path)
+                elif file.endswith(".docx"):
+                    loader = Docx2txtLoader(file_path)
 
-            else:
-                print(f"Skipped unsupported file: {file}")
-                continue
+                elif file.endswith(".txt"):
+                    loader = TextLoader(
+                        file_path,
+                        encoding="utf-8"
+                    )
 
-            docs = loader.load()
-            all_docs.extend(docs)
+                elif file.endswith(".csv"):
+                    loader = CSVLoader(file_path)
 
-            print(f"Loaded: {file}")
+                elif file.endswith(".xlsx"):
+                    loader = UnstructuredExcelLoader(file_path)
 
-        except Exception as e:
-            print(f"Error loading {file}: {str(e)}")
+                else:
+                    print(f"Skipped unsupported file: {file}")
+                    continue
 
-   
+                docs = loader.load()
+                all_docs.extend(docs)
+
+                print(f"Loaded: {file}")
+
+            except Exception as e:
+                print(f"Error loading {file}: {str(e)}")
+
     if not all_docs:
         print("No valid documents found.")
         return None
 
-    # documents data is split into chunks
+    
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200
@@ -70,11 +79,10 @@ def create_vectorstore():
 
     split_docs = splitter.split_documents(all_docs)
 
-    # gemini is used to create the mebeddings
+   
     embeddings = GoogleGenerativeAIEmbeddings(
         model="models/gemini-embedding-001"
     )
-
 
     vectorstore = FAISS.from_documents(
         split_docs,
